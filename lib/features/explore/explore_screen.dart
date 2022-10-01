964cd98e31fd9808c0d85641309314a2b8220b79
DELL <mahmoud180280@fci.bu.edu.eg>
2022-10-01 19:43:15 +0200

    mahmoud push

--- lib/features/explore/explore_screen.dart
+++ lib/features/explore/explore_screen.dart
@@ -3,6 +3,7 @@ import 'package:booking_app/core/main_blocs/blocs.dart';
 import 'package:booking_app/data/models/explore_model.dart';
 import 'package:booking_app/features/book_hotel.dart';
 import 'package:booking_app/features/home/cubit/app_states.dart';
+import 'package:booking_app/features/search_screen/search_screen.dart';
 import 'package:booking_app/resources/constants/constants.dart';
 import 'package:booking_app/resources/themes/theme.dart';
 import 'package:sizer/sizer.dart';
@@ -80,42 +81,50 @@ class _ExploreScreenState extends State<ExploreScreen> {
                       child: Row(
                         children: [
                           Expanded(
-                            child: TextFormField(
-                              controller: widget.searchController,
-                              keyboardType: TextInputType.text,
-                              onFieldSubmitted: (String value) {
-                                print(value);
+                            child: GestureDetector(
+                              onTap: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (_){
+                                  return SearchScreen();
+                                }));
                               },
-                              onChanged: (String value) {
-                                print(value);
-                              },
-                              style: TextStyle(
-                                  fontSize: 12.sp,
-                                  color:  OwnTheme.colorPalette['white'],
-                                  fontWeight: FontWeight.w500,
-                                  fontFamily: lang == "ar" ? "fontArBold" : "fontEnBold"
-                              ),
-                              decoration: InputDecoration(
-
-                                hintStyle: TextStyle(
-                                    fontSize: 11.sp,
-                                    color:  Colors.grey[100],
+                              child: TextFormField(
+                                enabled: false,
+                                // controller: widget.searchController,
+                                keyboardType: TextInputType.text,
+                                onFieldSubmitted: (String value) {
+                                  print(value);
+                                },
+                                onChanged: (String value) {
+                                  print(value);
+                                },
+                                style: TextStyle(
+                                    fontSize: 12.sp,
+                                    color:  OwnTheme.colorPalette['white'],
                                     fontWeight: FontWeight.w500,
                                     fontFamily: lang == "ar" ? "fontArBold" : "fontEnBold"
                                 ),
-                                hintText: 'London..',
-                                filled: true,
-                                fillColor: const Color(0xff282828),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                  borderSide: const BorderSide(
-                                    color: Color(0xff191919),
+                                decoration: InputDecoration(
+
+                                  hintStyle: TextStyle(
+                                      fontSize: 11.sp,
+                                      color:  Colors.grey[100],
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: lang == "ar" ? "fontArBold" : "fontEnBold"
                                   ),
-                                ),
-                                focusedBorder:  OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                  borderSide: const BorderSide(
-                                    color: Color(0xff191919),
+                                  hintText: 'London..',
+                                  filled: true,
+                                  fillColor: const Color(0xff282828),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: const BorderSide(
+                                      color: Color(0xff191919),
+                                    ),
+                                  ),
+                                  focusedBorder:  OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: const BorderSide(
+                                      color: Color(0xff191919),
+                                    ),
                                   ),
                                 ),
                               ),
@@ -124,7 +133,7 @@ class _ExploreScreenState extends State<ExploreScreen> {
                           SizedBox(width: 10,),
                           GestureDetector(
                             onTap: (){
-                              AppCubit.get(context).getSearchBooking(name: widget.searchController.text);
+                              // AppCubit.get(context).getSearchBooking(name: widget.searchController.text);
                             },
                             child: CircleAvatar(
                               radius: 30,
